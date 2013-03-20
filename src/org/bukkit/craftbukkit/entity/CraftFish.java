4642faa0ca344478fafd44f9291ad7490889aae6
Andre LeBlanc <andre@norcode.com>
2013-03-20 11:48:35 -0400

    Allow fishing success rate to be adjustable. Adds BUKKIT-3837
    Remapped by Srg2Source from Bukkit/CraftBukkit@afe50ba9d476b0e7bc0b60dcda78ae765bb5e366

--- src/org/bukkit/craftbukkit/entity/CraftFish.java
+++ src/org/bukkit/craftbukkit/entity/CraftFish.java
@@ -1,12 +1,15 @@
 package org.bukkit.craftbukkit.entity;
 
 
+import org.apache.commons.lang.Validate;
 import org.bukkit.craftbukkit.CraftServer;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Fish;
 import org.bukkit.entity.LivingEntity;
 
 public class CraftFish extends AbstractProjectile implements Fish {
+    private double biteChance = -1;
+
     public CraftFish(CraftServer server, net.minecraft.entity.projectile.EntityFishHook entity) {
         super(server, entity);
     }
@@ -37,5 +40,22 @@ public class CraftFish extends AbstractProjectile implements Fish {
 
     public EntityType getType() {
         return EntityType.FISHING_HOOK;
+    }
+
+    public double getBiteChance() {
+        net.minecraft.entity.projectile.EntityFishHook hook = getHandle();
+
+        if (this.biteChance == -1) {
+            if (hook.field_70170_p.func_72951_B(net.minecraft.util.MathHelper.func_76128_c(hook.field_70165_t), net.minecraft.util.MathHelper.func_76128_c(hook.field_70163_u) + 1, net.minecraft.util.MathHelper.func_76128_c(hook.field_70161_v))) {
+                return 1/300.0;
+            }
+            return 1/500.0;
+        }
+        return this.biteChance;
+    }
+
+    public void setBiteChance(double chance) {
+        Validate.isTrue(chance >= 0 && chance <= 1, "The bite chance must be between 0 and 1.");
+        this.biteChance = chance;
     }
 }
